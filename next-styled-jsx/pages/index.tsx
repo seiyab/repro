e245035e6431bc48e7d759762969c447876d1f85
seiya <20365512+seiyab@users.noreply.github.com>
2022-09-07 22:51:56 +0900

    try styled-jsx

--- next-styled-jsx/pages/index.tsx
+++ next-styled-jsx/pages/index.tsx
@@ -1,5 +1,12 @@
 function HomePage() {
-    return <div>Welcome to Next.js!</div>
+    return <div>
+        Welcome to Next.js!
+        <style jsx>{`
+            div {
+                color: red;
+            }
+        `}</style>
+    </div>
   }
   
   export default HomePage
